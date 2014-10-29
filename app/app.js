16edebe195f9997bdfc5f943ba81c721e2d0ad8c
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-10-29 23:51:30 +0000

    working form

--- app/app.js
+++ app/app.js
@@ -1,9 +1,34 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [])
+angular.module('myApp', [] )
 
 
+
+.controller('postCodeController', function(){
+  this.postcodes = postcodes
+  console.log(this.postcodes)
+  
+})
+
+
+.controller("submitController", function(){
+
+  this.postcode = {}
+  this.submitPostcode = function(inputPostcode){
+
+    console.log(postcodes)
+    postcodes.push(inputPostcode)
+    console.log(postcodes)
+    this.postcode = {}
+  }
+
+})
+
+var postcodes = [
+        {  postcodeDescription: "sdafasfdads"},
+        {  postcodeDescription: "george postcode" }
+      ]
 // config(['$routeProvider', function($routeProvider) {
 //   $routeProvider.otherwise({redirectTo: '/view1'});
 // }]);
